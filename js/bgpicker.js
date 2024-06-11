f9278320c6e10d338db3d13f0a60ee62a77e8a9f
Pibis <87157662+PibisFunny@users.noreply.github.com>
2024-06-11 16:35:36 -0700

    experimental font

--- js/bgpicker.js
+++ js/bgpicker.js
@@ -1,7 +1,8 @@
 function pic() {
 	var bgm= [
-'https://cdn.pixabay.com/photo/2015/01/28/23/35/hills-615429_960_720.jpg',
-'https://cdn.pixabay.com/photo/2016/10/22/17/46/mountains-1761292__340.jpg'
+'https://www.pibis.net/img/bg/bg_1.jpg',
+'https://www.pibis.net/img/bg/bg_2.jpg',
+'https://www.pibis.net/img/bg/bg_3.jpg',
 ];
 
 
